47380f8ec2df26f6cfdf0966ac5be95195f84d48
avivosh17 <s333119832@ariel.ort.org.il>
2025-01-09 09:00:43 +0200

    conected login to diary

--- website/pages/diary.ts
+++ website/pages/diary.ts
@@ -1,3 +1,5 @@
 import { send } from "../utilities";
 
 let savebutton = document.querySelector("#savebutton") as HTMLButtonElement;
+
+
--- website/pages/login.ts
+++ website/pages/login.ts
@@ -1,22 +1,19 @@
 import { send } from "../utilities";
 
-let usernameinput = document.querySelector("#usernameInput") as HTMLInputElement;
-let passwordinput = document.querySelector("#passwordInput") as HTMLInputElement;
-let submitbutton = document.querySelector("#submitButton") as HTMLButtonElement;
-let messagediv = document.querySelector("#messageDiv") as HTMLDivElement;
+let usernameinput = document.querySelector("#usernameinput") as HTMLInputElement;
+let passwordinput = document.querySelector("#passwordinput") as HTMLInputElement;
+let submitbutton = document.querySelector("#submitbutton") as HTMLButtonElement;
+let messagediv = document.querySelector("#messagediv") as HTMLDivElement;
 
 submitbutton.onclick = async function () {
     let userId = await send("login", [usernameinput.value, passwordinput.value]) as string | null;
 
     if (userId != null) {
-        localStorage.setItem("userId", userId);
+        localStorage.setItem("userid", userId);
 
-        location.href = "index.html";
+        location.href = "diary.html";
     }
     else {
         messagediv.innerText = "User does not exist.";
     }
-}
-submitbutton.onclick = async function () {
-
 }
